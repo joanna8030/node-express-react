60e034025c56a2cbe5a164f4a95570eda5b29889
Rebecca <joanna8030@gmail.com>
2017-01-24 17:58:40 +0800

    revise constants name

--- src/constants/uri.js
+++ src/constants/uri.js
@@ -1,3 +1,3 @@
-exports.ProductionApiUri = 'http://localhost:3000/issues/';
-exports.ProductionDBUri = 'mongodb://127.0.0.1:27017/Issues';
-exports.TestDBUri = 'mongodb://127.0.0.1:27017/test';
+exports.PRODUCTION_API_URI = 'http://localhost:3000/issues/';
+exports.PRODUCTION_DB_URI = 'mongodb://127.0.0.1:27017/Issues';
+exports.TEST_DB_URI = 'mongodb://127.0.0.1:27017/test';
