de961438896c0e90e45ce8b6de5fadc566eb0e14
Rebecca <joanna8030@gmail.com>
2017-01-26 00:31:30 +0800

    routing config

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import App from './containers/container';
+import { browserHistory, Router, Route } from 'react-router';
 import reducer from './reducers';
+import App from './containers/container';
+import NewForm from './components/new-issue';
+import UpdateForm from './components/update-issue';
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path='/' component={App} />
+      <Route path='/new' component={NewForm} />
+      <Route path='/update/:id' component={UpdateForm} />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
