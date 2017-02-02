81e171e25a6ad291c74e24c0e73bc7870aa98cb8
Rebecca <joanna8030@gmail.com>
2017-02-02 16:04:50 +0800

    put port number in config.js

--- src/constants/uri.js
+++ src/constants/uri.js
@@ -1,3 +1,5 @@
-exports.PRODUCTION_API_URI = 'http://localhost:3000/issues/';
+import { API_PORT } from '../../config';
+
+exports.PRODUCTION_API_URI = 'http://localhost:' + API_PORT + '/issues/';
 exports.PRODUCTION_DB_URI = 'mongodb://127.0.0.1:27017/Issues';
 exports.TEST_DB_URI = 'mongodb://127.0.0.1:27017/test';
